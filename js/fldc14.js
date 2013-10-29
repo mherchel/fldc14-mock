aca711bccbf1eb3a0c55abd98d9d35bcf285eecb
Mike Herchel <mike@herchel.com>
2013-10-29 15:53:25 -0400

    Adding a news.html and some more changes.

--- js/fldc14.js
+++ js/fldc14.js
@@ -5,11 +5,14 @@ $(document ).ready(function() {
 		$( "#heading" ).addClass( "scrolldown" );
 		$( "#heading h1 a" ).addClass( "scrolldown" );
 		$( "#mobile-menu" ).addClass( "scrolldown" );
+		$( "#login-links" ).addClass( "scrolldown" );
+		
 	}
 	if($(window).scrollTop() < $('#heading').outerHeight()) {
 		$( "#heading" ).removeClass( "scrolldown" );
 		$( "#heading h1 a" ).removeClass( "scrolldown" );
 		$( "#mobile-menu" ).removeClass( "scrolldown" );
+		$( "#login-links" ).removeClass( "scrolldown" );
 	}	
 	});
 
