e485118a774cbc934f81b773523d4ae370c6a591
Mike Herchel <mike@herchel.com>
2013-10-29 13:12:56 -0400

    Refactoring for mobile.

--- js/fldc14.js
+++ js/fldc14.js
@@ -3,9 +3,13 @@ $(document ).ready(function() {
 	$(window).scroll(function(){
 	if($(window).scrollTop() >= $('#heading').outerHeight()) {
 		$( "#heading" ).addClass( "scrolldown" );
+		$( "#heading h1 a" ).addClass( "scrolldown" );
+		$( "#mobile-menu" ).addClass( "scrolldown" );
 	}
 	if($(window).scrollTop() < $('#heading').outerHeight()) {
 		$( "#heading" ).removeClass( "scrolldown" );
+		$( "#heading h1 a" ).removeClass( "scrolldown" );
+		$( "#mobile-menu" ).removeClass( "scrolldown" );
 	}	
 	});
 
